0f3367ff3b83bc5d2ddada14a7484f478caddf3c
Jairo Vasquez <jairo.vasquez@gmail.com>
2011-09-11 22:41:58 -0300

    Serve Index page

--- urls.py
+++ urls.py
@@ -1,17 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
+# Enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'teachcork.views.home', name='home'),
-    # url(r'^teachcork/', include('teachcork.foo.urls')),
+urlpatterns = patterns('app.views',
+    url(r'^$', 'index', name='index'),
+)
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+urlpatterns += patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
