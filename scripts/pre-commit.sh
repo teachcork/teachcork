5eafeda2cdf6f1933768c51a62ac587cdbee739c
Jairo Vasquez <jairo.vasquez@gmail.com>
2012-02-01 02:34:15 -0200

    Adding gae folder to pre-commit script

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -18,10 +18,10 @@ production_checks(){
     tput sgr0
 
     # Should pass pep8
-    PEP=$(pep8 --ignore=E501,E221,E202,E251,W391,W291,E701,W293 --repeat app scripts | wc -l);
+    PEP=$(pep8 --ignore=E501,E221,E202,E251,W391,W291,E701,W293 --repeat gae app scripts | wc -l);
     if [ $PEP -gt 0 ];
     then
-	pep8 --ignore=E501,E221,E202,E251,W391,W291,E701,W293 --repeat app scripts
+	pep8 --ignore=E501,E221,E202,E251,W391,W291,E701,W293 --repeat gae app scripts
 	echo "$(tput bold) $(tput setaf 1) Commit failed. Check the PEP8 results and try again $(tput sgr0)"
 	exit 1
     else
@@ -32,7 +32,7 @@ production_checks(){
     echo "$(tput setaf 1)"
 
     #exclude __init__.py's from pyflakes so we can import w/o using on modules
-    files=$(find app scripts -name "*.py" -not -name "__init__.py")
+    files=$(find gae app scripts -name "*.py" -not -name "__init__.py")
     if pyflakes $files;
     then
 	echo "$(tput sgr0)"
